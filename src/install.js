3121fd01a312a44074e519f22d0efdab27184bf9
Tomer Shalev <tshalev@proofpoint.com>
2020-03-07 21:48:49 +0200

    Move store

--- src/install.js
+++ src/install.js
@@ -5,23 +5,15 @@
  * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/InstallAPI.js
  */
 
-const extendConf = function (conf) {
-    // make sure google-maps boot file is registered
-    conf.boot.push('~@quasar/quasar-app-extension-geolocation/src/boot/google-maps.js')
-    console.log(` App Extension (qwindow) Info: 'Adding qwindow boot reference to your quasar.conf.js'`)
-
-    // make sure boot & component files transpile
-    conf.build.transpileDependencies.push(/quasar-app-extension-geolocation[\\/]src/)
-}
-
 module.exports = function (api) {
-    api.extendPackageJson({
-        dependencies: {
-            "vue2-google-maps": "^0.10.7"
-        }
-    })
-    api.render('./templates')
+    // TODO: verify the Vuex is installed
 
-    // extend quasar.conf
-    api.extendQuasarConf(extendConf)
+    if (api.prompts.installVueGoogleMaps) {
+        api.extendPackageJson({
+            dependencies: {
+                "vue2-google-maps": "^0.10.7"
+            }
+        })
+    }
+    api.render('./templates')
 }
